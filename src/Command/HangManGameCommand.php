691324640a6c7f7feecc6375ba2f821235e09a28
Juan Manuel Torres <kinojman@yahoo.com>
2015-04-14 02:16:09 +0000

    First commit

---
+++ src/Command/HangManGameCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SDPHP\HangMan;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+class HangManGameCommand extends Command
+{
+    protected function configure()
+    {
+
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+       
+    }
+}
